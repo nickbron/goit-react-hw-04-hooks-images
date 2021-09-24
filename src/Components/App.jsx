efb4860501f55d277f77b3210d8ac852cdabcb46
nickbron <77615583+nickbron@users.noreply.github.com>
2021-09-24 09:21:56 +0300

    create hook

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,6 +1,6 @@
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import { Searchbar } from './Searchbar/Searchbar';
 import { fetchPictures } from 'Services/api';
 import { ImageGallery } from './ImageGallery/ImageGallery';
@@ -8,17 +8,14 @@ import { LoadMoreBtn } from './Button/Button';
 import Modal from './Modal/Modal';
 import Spinner from './Spinner/Spinner';
 
-export default class App extends Component {
-  state = {
-    pictureName: null,
-    pictures: [],
-    page: 1,
-    selectedImage: null,
-    showModal: false,
-  };
+export default function App() {
+  const [pictureName, setPictureName] = useState(null);
+  const [pictures, setPictures] = useState([]);
+  const [page, setPage] = useState(1);
+  const [selectedImage, setSelectedImage] = useState(null);
+  const [showModal, setShowModal] = useState(false);
 
-  fetchData = async () => {
-    const { pictureName, page } = this.state;
+  const fetchData = async () => {
     const data = await fetchPictures(pictureName, page);
     const imageInfo = data.map(({ id, largeImageURL, webformatURL }) => ({
       id,
@@ -29,77 +26,170 @@ export default class App extends Component {
     return imageInfo;
   };
 
-  componentDidUpdate(prevProps, prevState) {
-    const imageInfo = this.fetchData();
-
-    this.scroll();
-    if (prevState.pictureName !== this.state.pictureName) {
-      imageInfo.then(res => {
-        this.setState({ pictures: res });
-      });
-    }
-
-    if (prevState.page !== this.state.page) {
-      imageInfo.then(res => {
-        this.setState({ pictures: [...prevState.pictures, ...res] });
-      });
-    }
-  }
-
-  scroll = () => {
+  const scroll = () => {
     window.scrollTo({
       top: document.documentElement.scrollHeight,
       behavior: 'smooth',
     });
   };
 
-  toggleModal = () => {
-    this.setState(state => ({ showModal: !state.showModal }));
+  const toggleModal = () => {
+    setShowModal(!showModal);
   };
 
-  handleBarSubmit = pictureName => {
-    this.setState({ pictureName });
+  const handleBarSubmit = pictureName => {
+    setPictureName(pictureName);
   };
 
-  handleSelectImage = selectedImage => {
-    this.setState({ selectedImage });
-    this.toggleModal();
+  const handleSelectImage = selectedImage => {
+    setSelectedImage(selectedImage);
+    toggleModal();
   };
 
-  showMore = () => {
-    this.setState(prevState => ({ page: prevState.page + 1 }));
+  const showMore = () => {
+    setPage(prevPage => prevPage + 1);
   };
 
-  render() {
-    const { pictures, showModal, selectedImage } = this.state;
+  useEffect(() => {
+    const imageInfo = fetchData();
+    scroll();
 
-    return (
-      <div className="App">
-        <Searchbar onSearch={this.handleBarSubmit} />
-        {pictures.length > 0 && (
-          <>
-            <Spinner />
-            <ImageGallery pic={pictures} onSelect={this.handleSelectImage} />;
-            <LoadMoreBtn onClick={this.showMore} />
-            {showModal && (
-              <Modal onClose={this.toggleModal}>
-                <img src={selectedImage} alt="" />
-              </Modal>
-            )}
-          </>
-        )}
-        <ToastContainer
-          position="top-right"
-          autoClose={3000}
-          hideProgressBar={false}
-          newestOnTop={false}
-          closeOnClick
-          rtl={false}
-          pauseOnFocusLoss
-          draggable
-          pauseOnHover
-        />
-      </div>
-    );
-  }
+    imageInfo.then(res => {
+      setPictures(res);
+    });
+  }, [pictureName]);
+
+  useEffect(() => {
+    const imageInfo = fetchData();
+    scroll();
+
+    imageInfo.then(res => {
+      setPictures(prevPictures => [...prevPictures, ...res]);
+    });
+  }, [page]);
+
+  return (
+    <div className="App">
+      <Searchbar onSearch={handleBarSubmit} />
+      {pictures.length > 0 && (
+        <>
+          <Spinner />
+          <ImageGallery pic={pictures} onSelect={handleSelectImage} />;
+          <LoadMoreBtn onClick={showMore} />
+          {showModal && (
+            <Modal onClose={toggleModal}>
+              <img src={selectedImage} alt="" />
+            </Modal>
+          )}
+        </>
+      )}
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
+    </div>
+  );
 }
+
+// export default class App extends Component {
+//   state = {
+//     pictureName: null,
+//     pictures: [],
+//     page: 1,
+//     selectedImage: null,
+//     showModal: false,
+//   };
+
+//   fetchData = async () => {
+//     const { pictureName, page } = this.state;
+//     const data = await fetchPictures(pictureName, page);
+//     const imageInfo = data.map(({ id, largeImageURL, webformatURL }) => ({
+//       id,
+//       largeImageURL,
+//       webformatURL,
+//     }));
+
+//     return imageInfo;
+//   };
+
+//   componentDidUpdate(prevProps, prevState) {
+//     const imageInfo = this.fetchData();
+
+//     this.scroll();
+//     if (prevState.pictureName !== this.state.pictureName) {
+//       imageInfo.then(res => {
+//         this.setState({ pictures: res });
+//       });
+//     }
+
+//     if (prevState.page !== this.state.page) {
+//       imageInfo.then(res => {
+//         this.setState({ pictures: [...prevState.pictures, ...res] });
+//       });
+//     }
+//   }
+
+//   scroll = () => {
+//     window.scrollTo({
+//       top: document.documentElement.scrollHeight,
+//       behavior: 'smooth',
+//     });
+//   };
+
+//   toggleModal = () => {
+//     this.setState(state => ({ showModal: !state.showModal }));
+//   };
+
+//   handleBarSubmit = pictureName => {
+//     this.setState({ pictureName });
+//   };
+
+//   handleSelectImage = selectedImage => {
+//     this.setState({ selectedImage });
+//     this.toggleModal();
+//   };
+
+//   showMore = () => {
+//     this.setState(prevState => ({ page: prevState.page + 1 }));
+//   };
+
+//   render() {
+//     const { pictures, showModal, selectedImage } = this.state;
+
+//     return (
+//       <div className="App">
+//         <Searchbar onSearch={this.handleBarSubmit} />
+//         {pictures.length > 0 && (
+//           <>
+//             <Spinner />
+//             <ImageGallery pic={pictures} onSelect={this.handleSelectImage} />;
+//             <LoadMoreBtn onClick={this.showMore} />
+//             {showModal && (
+//               <Modal onClose={this.toggleModal}>
+//                 <img src={selectedImage} alt="" />
+//               </Modal>
+//             )}
+//           </>
+//         )}
+//         <ToastContainer
+//           position="top-right"
+//           autoClose={3000}
+//           hideProgressBar={false}
+//           newestOnTop={false}
+//           closeOnClick
+//           rtl={false}
+//           pauseOnFocusLoss
+//           draggable
+//           pauseOnHover
+//         />
+//       </div>
+//     );
+//   }
+// }
--- src/Components/Modal/Modal.jsx
+++ src/Components/Modal/Modal.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { createPortal } from 'react-dom';
 
@@ -6,36 +6,63 @@ import { ModalWindow, Overlay } from './Modal.styled';
 
 const modalRoot = document.querySelector('#modal-root');
 
-export default class Modal extends Component {
-  componentDidMount() {
-    window.addEventListener('keydown', this.onKeyDown);
-  }
+export default function Modal(props) {
+  useEffect(() => {
+    window.addEventListener('keydown', onKeyDown);
+    return () => {
+      window.removeEventListener('keydown', onKeyDown);
+    };
+  });
 
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.onKeyDown);
-  }
-
-  onKeyDown = e => {
+  const onKeyDown = e => {
     if (e.code === 'Escape') {
-      this.props.onClose();
-    }
-  };
-  onOverlayClick = e => {
-    if (e.target === e.currentTarget) {
-      this.props.onClose();
+      props.onClose();
     }
   };
 
-  render() {
-    return createPortal(
-      <Overlay onClick={this.onOverlayClick}>
-        <ModalWindow>{this.props.children}</ModalWindow>
-      </Overlay>,
-      modalRoot,
-    );
-  }
+  const onOverlayClick = e => {
+    if (e.target === e.currentTarget) {
+      props.onClose();
+    }
+  };
+  return createPortal(
+    <Overlay onClick={onOverlayClick}>
+      <ModalWindow>{props.children}</ModalWindow>
+    </Overlay>,
+    modalRoot,
+  );
 }
 
-Modal.propTypes = {
-  onClose: PropTypes.func.isRequired,
-};
+// export default class Modal extends Component {
+//   componentDidMount() {
+//     window.addEventListener('keydown', this.onKeyDown);
+//   }
+
+//   componentWillUnmount() {
+//     window.removeEventListener('keydown', this.onKeyDown);
+//   }
+
+//   onKeyDown = e => {
+//     if (e.code === 'Escape') {
+//       this.props.onClose();
+//     }
+//   };
+//   onOverlayClick = e => {
+//     if (e.target === e.currentTarget) {
+//       this.props.onClose();
+//     }
+//   };
+
+//   render() {
+//     return createPortal(
+//       <Overlay onClick={this.onOverlayClick}>
+//         <ModalWindow>{this.props.children}</ModalWindow>
+//       </Overlay>,
+//       modalRoot,
+//     );
+//   }
+// }
+
+// Modal.propTypes = {
+//   onClose: PropTypes.func.isRequired,
+// };
